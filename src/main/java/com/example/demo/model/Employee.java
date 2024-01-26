bd7ee1805e908a8aa1e112eaf840101f920773db
Aditya <adityaoswal529@gmail.com>
2024-01-26 13:11:12 +0530

    first commit

---
+++ src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,35 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+
+public class Employee {
+
+	@Id
+	private int eid;
+	private String address;
+	private String ename;
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/example/demo/service/HomeService.java
@@ -0,0 +1,22 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.model.Employee;
+
+
+public interface HomeService {
+
+public	Employee save(Employee e);
+
+public List getd();
+
+public Employee updated(Employee e);
+
+public void deletedata(int a);
+
+
+
+
+
+}
